edc129217694d1b3ca2d4057c17b92f9bfbaaf91
jv <12345678+username@users.noreply.github.com>
2024-06-04 06:37:21 +0800

    project finished, created computerChoice function that will return random number and change the innerhtml according to the random numnber. created the gameMechanics function that will process the logic of the game. and lastly created playerChoice function that will change the innerhtml according to the player choice also in this function I consider this as the main function where all the function of the script are called and all the process of the game are made

--- script.js
+++ script.js
@@ -1,16 +1,119 @@
-
+let playerSign;
+let playerScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
 
 function playerChoice(choice){
-    console.log(choice);
+
+    computerChoice();
     switch(choice.id){
         case "btn-rock":
             document.getElementById("player-sign").innerHTML = "✊";
+            playerSign = 0;
             break;
         case "btn-paper":
-            document.getElementById("player-sign").innerHTML = "✋";
+             document.getElementById("player-sign").innerHTML = "✋";
+            playerSign = 1;
+
             break;
         case "btn-scissor":
             document.getElementById("player-sign").innerHTML = "✌";
+            playerSign = 2;
             break;
     }
+
+    let result = gameMechanics(playerSign,computerChoice());
+    if(result=="draw"){
+        document.getElementById("round-counter").innerHTML = "Round "+roundCounter+": Draw";
+    }
+    if(result=="player"){
+        document.getElementById("round-counter").innerHTML = "Round "+roundCounter+": You win this round!";
+    }
+    if(result=="computer"){
+        document.getElementById("round-counter").innerHTML = "Round "+roundCounter+": You lose this round!";
+    }
+
+
+
+    if(playerScore == 5){
+        alert("You win!");
+        playerScore = 0;
+        computerScore = 0;
+        roundCounter = 1;
+        document.getElementById("round-counter").innerHTML = "Choose your weapon";
+        document.getElementById("player-sign").innerHTML = "❔";
+        document.getElementById("computer-sign").innerHTML = "❔";
+
+
+    }
+    else if(computerScore == 5)
+    {
+        alert("You lose!");
+        playerScore = 0;
+        computerScore = 0;
+        roundCounter = 1;
+        document.getElementById("round-counter").innerHTML = "Choose your weapon";
+        document.getElementById("player-sign").innerHTML = "❔";
+        document.getElementById("computer-sign").innerHTML = "❔";
+    }
+    document.getElementById("player-score").innerHTML = "Score : "+playerScore;
+    document.getElementById("computer-score").innerHTML = "Score : "+computerScore;
+
+
+}
+function computerChoice(){
+    let compSign = Math.floor(Math.random() * 3);
+    switch(compSign){
+        case 0:
+            document.getElementById("computer-sign").innerHTML = "✊";
+            break;
+        case 1:
+            document.getElementById("computer-sign").innerHTML = "✋";
+            break;
+        case 2:
+            document.getElementById("computer-sign").innerHTML = "✌";
+            break;
+    }
+
+    return compSign;
+}
+
+function gameMechanics(playerChoice,compChoice){
+    let result;
+    if(playerChoice == 0 && compChoice == 0){
+        result = "draw";
+    }
+    if(playerChoice == 0 && compChoice == 1){
+        computerScore++;
+        result = "computer";
+    }
+    if(playerChoice == 0 && compChoice == 2){
+        playerScore++;
+        result = "player"
+    }
+    if(playerChoice == 1 && compChoice == 0){
+        playerScore++;
+        result = "player"
+    }
+    if(playerChoice == 1 && compChoice == 1){
+        result = "draw";
+    }
+    if(playerChoice == 1 && compChoice == 2){
+        computerScore++;
+        result = "computer";
+    }
+    if(playerChoice == 2 && compChoice == 0){
+        computerScore++;
+        result = "computer";
+    }
+    if(playerChoice == 2 && compChoice == 1){
+        playerScore++;
+        result = "player"
+    }
+    if(playerChoice == 2 && compChoice == 2){
+        result = "draw";
+    }
+
+    roundCounter++;
+    return result;
 }
